21551e8d21aaf922006a1ef33e428d17965ff4b6
Sungho Hwang <sam.hwang@hanwha.com>
2021-08-16 03:01:48 +0900

    Time: 8 ms (88.51%), Space: 6.7 MB (89.24%) - LeetHub

---
+++ distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,30 @@
+/*
+dp[i][j]: s[..j] 에 있는 t[..i] 의 subsequence 의 개수
+Answer: dp[N][M] (N: len(t), N: len(s))
+*/
+typedef long long ll;
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int N = t.size();
+        int M = s.size();
+        vector<vector<ll>> dp = vector<vector<ll>>(2, vector<ll>(M + 1, 0));
+        for(int i = 0; i <= max(0, M - N); i++) {
+            dp[0][i] = 1;
+        }
+        for(int i = 1; i<= N; i++) {
+            for(int j = 1; j <= M - (N - i); j++) {
+                dp[i & 1][j] = dp[i & 1][j - 1];
+                if(t[i - 1] == s[j - 1]) {
+                    dp[i & 1][j] += dp[~i & 1][j - 1];
+                }
+                
+            }
+            if(i == 1) {
+                dp[~i & 1][0] = 0;
+            }
+        }
+        return dp[N & 1][M];
+    }
+};

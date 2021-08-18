99908bf0b49de992f8b36421add0bc5535a17530
Sungho Hwang <sam.hwang@hanwha.com>
2021-08-19 02:15:30 +0900

    Time: 16 ms (92.99%), Space: 28.8 MB (56.35%) - LeetHub

--- subtree-of-another-tree/subtree-of-another-tree.cpp
+++ subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -12,8 +12,6 @@
 typedef long long ll;
 
 class Solution {
-    ll MOD = 1e9+7;
-    ll hashes[10003];
     bool compare(TreeNode* t1, TreeNode* t2) {
         if((t1 == nullptr) != (t2 == nullptr)) return false;
         if(t1 == nullptr) return true;
@@ -22,27 +20,14 @@ class Solution {
             && compare(t1->right, t2->right);
     }
     
-    pair<ll, pair<int, bool>> find(TreeNode* cur, TreeNode* target, ll targetHash) {
-        if(cur == nullptr) return {0, {0, false}};
-        auto left = find(cur->left, target, targetHash);
-        auto right = find(cur->right, target, targetHash);
-        if(left.second.second || right.second.second) return {0, {0, true}};
-        ll newHash = (hashes[right.second.first + 1] * left.first + right.first * 20002 + (cur->val + 10001)) % MOD;
-        bool found = false;
-        if(newHash == targetHash) {
-            found = compare(cur, target);
-        }
-        return {newHash, {left.second.first + right.second.first + 1, found}};
+    bool dfs(TreeNode* cur, TreeNode* target) {
+        if(cur == nullptr) return false;
+        return dfs(cur->left, target) ||
+            dfs(cur->right, target) ||
+            compare(cur, target);
     }
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        hashes[0] = 1;
-        for(int i = 1; i <= 10001; i++) {
-            hashes[i] = hashes[i - 1] * 20002 % MOD;
-        }
-        auto p = find(subRoot, nullptr, -1);
-        ll targetHash = p.first;
-        auto ret = find(root, subRoot, targetHash);
-        return ret.second.second;
+        return dfs(root, subRoot);
     }
 };

800661f5d16c88b65aab7ade9bbf7812c83580c6
Sungho Hwang <sam.hwang@hanwha.com>
2021-08-20 03:35:22 +0900

    Time: 0 ms (100.00%), Space: 7 MB (92.83%) - LeetHub

--- rotate-image/rotate-image.cpp
+++ rotate-image/rotate-image.cpp
@@ -14,17 +14,9 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         N = matrix.size();
         int half = N / 2;
-        if(N % 2 == 0) {
-            for(int i = 0; i < half; i++) {
-                for(int j = 0; j < half; j++) {
-                    rotate4(matrix, i, j);
-                }
-            }
-        } else {
-            for(int i = 0; i < half; i++) {
-                for(int j = i; j < N - 1 - i; j++) {
-                    rotate4(matrix, i, j);
-                }
+        for(int i = 0; i < half; i++) {
+            for(int j = i; j < N - 1 - i; j++) {
+                rotate4(matrix, i, j);
             }
         }
     }

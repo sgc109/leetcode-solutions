5aa8b83788c75329a05010dbe4aa1f3769a902e9
Sungho Hwang <sam.hwang@hanwha.com>
2021-08-22 03:21:02 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (85.39%) - LeetHub

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -3,16 +3,12 @@ typedef long long ll;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        if(m < n) return uniquePaths(n, m);
         m--, n--;
         int sum = m + n;
-        int mn = min(m, n);
-        vector<int> nums;
         ll ans = 1;
-        for(int i = 0; i < mn; i++) {
-            ans *= sum - i;
-        }
-        for(int i = mn; i >= 2; i--) {
-            ans /= i;
+        for(int i = 0; i < n; i++) {
+            ans = (ans * (sum - i) / (i + 1));
         }
         return ans;
     }

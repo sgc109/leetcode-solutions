38194d58bd9d0727035a8321d852f745e5d348ae
Sungho Hwang <sam.hwang@hanwha.com>
2021-08-22 02:42:59 +0900

    Time: 44 ms (39.73%), Space: 20.6 MB (45.70%) - LeetHub

---
+++ group-anagrams/group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> dict;
+        for(auto str : strs) {
+            string sorted = str;
+            sort(begin(sorted), end(sorted));
+            if(dict.count(sorted) == 0) {
+                dict[sorted] = vector<string>();
+            }
+            dict[sorted].push_back(str);
+        }
+        vector<vector<string>> ret;
+        for(auto kv : dict) {
+            ret.push_back(kv.second);
+        }
+        return ret;
+    }
+};

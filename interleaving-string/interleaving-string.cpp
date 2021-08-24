6f6b40d22f30e6ed381466ed7f28f5c8cdddaf9f
Sungho Hwang <sam.hwang@hanwha.com>
2021-08-25 01:53:09 +0900

    Time: 4 ms (74.42%), Space: 6.2 MB (85.93%) - LeetHub

--- interleaving-string/interleaving-string.cpp
+++ interleaving-string/interleaving-string.cpp
@@ -1,27 +1,18 @@
 class Solution {
-    int dp[103][103];
-    int solve(int pos1, int pos2, string& s1, string& s2, string& s3) {
-        if(pos1 > s1.size()) return false;
-        if(pos2 > s2.size()) return false;
-        if(pos1 + pos2 == s3.size()) return true;
-        int& cache = dp[pos1][pos2];
-        if(cache != -1) return cache;
-        int ret = 0;
-        if(pos1 < s1.size() 
-           && s1[pos1] == s3[pos1 + pos2] 
-           && solve(pos1 + 1, pos2, s1, s2, s3)) return cache = 1;
-        if(pos2 < s2.size()
-           && s2[pos2] == s3[pos1 + pos2]
-           && solve(pos1, pos2 + 1, s1, s2, s3)) return cache = 1;
-        
-        return cache = 0;
-    }
+    bool dp[101][101];
 public:
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size() + s2.size() != s3.size()) {
             return false;
         }
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, s1, s2, s3);
+        for(int i = 0; i <= s1.size(); i++) {
+            for(int j = 0; j <= s2.size(); j++) {
+                dp[i][j] = ((i == 0 && j == 0)
+                            || (i > 0 && s1[i - 1] == s3[i + j - 1] && dp[i - 1][j]) 
+                            || (j > 0 && s2[j - 1] == s3[i + j - 1] && dp[i][j - 1]));
+            }
+        }
+        
+        return dp[s1.size()][s2.size()];
     }
 };

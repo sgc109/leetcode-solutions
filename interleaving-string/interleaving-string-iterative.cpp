4b554b034c22b3d722cdfa3fdc0acdec4e53dd8f
Sungho Hwang <sam.hwang@hanwha.com>
2021-08-25 01:54:50 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (85.93%) - LeetHub

--- interleaving-string/interleaving-string-iterative.cpp
+++ interleaving-string/interleaving-string-iterative.cpp
@@ -1,3 +1,15 @@
+/*
+N: len(s1)
+M: len(s2) 일 때,
+
+Time Complexity: O(NM)
+Space Complexity: O(NM)
+
+dp[i][j]: s1[..i] 와 s2[..j] 를 interleaving 해서 s3[..i+j] 를 만들 수 있는지 여부(1-base indexing 기준)
+
+s1 과 s2 각각에 대해 포인터를 두고, s3[i+j] 와 맞출 문자로 현재 두 포인터가 각각 가리키는 두 문자 중 하나를 고를 수 있으므로, 아래와 같은 점화식이 나온다.
+*/
+
 class Solution {
     bool dp[101][101];
 public:

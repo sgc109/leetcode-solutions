36b9797114c08edd6b36224e2a13e80ac04c6c20
Sungho Hwang <sgc109109@gmail.com>
2021-11-05 22:09:16 +0900

    Time: 12 ms (63.94%), Space: 6.9 MB (84.98%) - LeetHub

--- longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,17 +1,14 @@
-class Solution {    
+class Solution {
     int solve(char target, string& s, int k) {
         int l = 0;
         int diffCnt = 0;
-        int sameCnt = 0;
         int ans = 0;
         for(int r = 0; r < s.size(); ++r) {
             diffCnt += (s[r] != target);
-            ++sameCnt;
             while(diffCnt > k) {
                 diffCnt -= (s[l++] != target);
-                --sameCnt;
             }
-            ans = max(ans, sameCnt);
+            ans = max(ans, r - l + 1);
         }
         
         return ans;

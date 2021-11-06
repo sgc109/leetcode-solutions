dbc20123afc452fc38f6d67a915ec57b299db5a3
Sungho Hwang <sgc109109@gmail.com>
2021-11-06 20:56:58 +0900

    Time: 4 ms (98.86%), Space: 11.6 MB (51.54%) - LeetHub

--- shortest-word-distance/shortest-word-distance.cpp
+++ shortest-word-distance/shortest-word-distance.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
-        vector<int> poses1, poses2;
+        int ans = 30000;
+        int p1 = -1, p2 = -1;
         for(int i = 0; i < wordsDict.size(); ++i) {
             string word = wordsDict[i];
             if(word == word1) {
-                poses1.push_back(i);
+                p1 = i;
             } else if(word == word2) {
-                poses2.push_back(i);
+                p2 = i;
+            }
+            if(p1 != -1 && p2 != -1) {
+                ans = min(ans, abs(p1 - p2));
             }
         }
-        int ans = 30000;
-        int p1 = 0, p2 = 0;
-        while(p1 < poses1.size() && p2 < poses2.size()) {
-            int idx1 = poses1[p1];
-            int idx2 = poses2[p2];
-            ans = min(ans, abs(idx1 - idx2));
-            if(idx1 < idx2) {
-                ++p1;
-            } else if(idx1 > idx2) {
-                ++p2;
-            } else {
-                assert(false);
-            }
-        }
+        
         return ans;
     }
 };

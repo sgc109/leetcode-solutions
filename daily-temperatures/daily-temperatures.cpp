7136669a83014488d093a87ca818438b444a284d
Sungho Hwang <sam.hwang@hanwha.com>
2021-11-15 02:39:25 +0900

    Time: 116 ms (98.19%), Space: 85.1 MB (70.10%) - LeetHub

--- daily-temperatures/daily-temperatures.cpp
+++ daily-temperatures/daily-temperatures.cpp
@@ -1,35 +1,20 @@
 class Solution {
-    const int MAX_IDX = 100001;
-    int minIdx[111];
-    int sqrtMinIdx[12];
-    
-    int query(int l) { // inclusive
-        int sq = (l+9) / 10;
-        int ans = MAX_IDX;
-        for(int i = l; i < sq*10; ++i) {
-            ans = min(ans, minIdx[i]);
-        }
-        for(int i = sq; i <= 10; ++i) {
-            ans = min(ans, sqrtMinIdx[i]);
-        }
-        return ans;
-    }
-    
-    void update(int idx, int val) {
-        minIdx[idx] = val;
-        sqrtMinIdx[idx/10] = val;
-    }
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        fill(minIdx, minIdx+111, MAX_IDX);
-        fill(sqrtMinIdx, sqrtMinIdx+12, MAX_IDX);
         int N = temperatures.size();
         vector<int> ans(N);
+        vector<int> stk;
         for(int i = N-1; i >= 0; --i) {
             int t = temperatures[i];
-            int q = query(t + 1);
-            ans[i] = (q == MAX_IDX ? 0 : q - i);
-            update(t, i);
+            while(!stk.empty() && t >= temperatures[stk.back()]) {
+                stk.pop_back();
+            }
+            if(stk.size() == 0) {
+                ans[i] = 0;
+            } else {
+                ans[i] = stk.back() - i;
+            }
+            stk.push_back(i);
         }
         return ans;
     }

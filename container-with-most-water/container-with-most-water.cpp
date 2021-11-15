90324d350222addd22580dc7cf1016c9e771e4d0
Sungho Hwang <sam.hwang@hanwha.com>
2021-11-16 00:57:26 +0900

    Time: 88 ms (55.93%), Space: 59 MB (80.43%) - LeetHub

---
+++ container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        
+        int ans = 0;
+        while(l < r) {
+            ans = max(ans, (r - l) * min(height[l], height[r]));
+            if(height[l] < height[r]) l++;
+            else r--;
+        }
+        
+        return ans;
+    }
+};

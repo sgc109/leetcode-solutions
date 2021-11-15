082e835debed88cf46d0d119ea52c7d77d753c7d
Sungho Hwang <sgc109109@gmail.com>
2021-11-15 18:11:59 +0900

    Time: 0 ms (100.00%), Space: 5.8 MB (98.34%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.cpp
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,19 +1,19 @@
 class Solution {
-    int dp[20][20];
-    int countBsts(int l, int r) { // [l, r)
-        assert(l <= r);
-        if(l >= r-1) return 1;
-        int& cache = dp[l][r];
-        if(cache != -1) return dp[l][r];
+    int dp[20];
+    int countBsts(int len) {
+        assert(len >= 0);
+        if(len <= 1) return 1;
+        int& cache = dp[len];
+        if(cache != -1) return dp[len];
         int ret = 0;
-        for(int k = l; k < r; ++k) {
-            ret += countBsts(l, k) * countBsts(k+1, r);
+        for(int k = 0; k < len; ++k) {
+            ret += countBsts(k) * countBsts(len-k-1);
         }
         return cache = ret;
     }
 public:
     int numTrees(int n) {
         memset(dp, -1, sizeof(dp));
-        return countBsts(0, n);
+        return countBsts(n);
     }
 };

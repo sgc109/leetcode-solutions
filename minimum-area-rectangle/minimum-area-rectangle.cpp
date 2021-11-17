dda9b12ae52a7d9554d9bf9bcfdc881c122cf82d
Sungho Hwang <sam.hwang@hanwha.com>
2021-11-17 20:28:39 +0900

    Time: 584 ms (46.20%), Space: 23.4 MB (34.42%) - LeetHub

--- minimum-area-rectangle/minimum-area-rectangle.cpp
+++ minimum-area-rectangle/minimum-area-rectangle.cpp
@@ -1,48 +1,23 @@
 class Solution {
-    const int INF = INT_MAX;
-    struct Point {
-        int x;
-        int y;
-    };
-    
-    map<int, vector<Point>> groupedByX;
-    map<int, vector<Point>> groupedByY;
-    unordered_set<int> pts;
-    Point restPoint(Point& a, Point& b, Point& c) {
-        return Point{a.x ^ b.x ^ c.x, a.y ^ b.y ^ c.y};
-    }
+    unordered_map<int, unordered_set<int>> x2y;
 public:
     int minAreaRect(vector<vector<int>>& points) {
         for(auto p : points) {
-            int xx = p[0];
-            int yy = p[1];
-            if(groupedByX.find(xx) == groupedByX.end()) {
-                groupedByX[xx] = vector<Point>();
-            }
-            if(groupedByY.find(yy) == groupedByY.end()) {
-                groupedByY[yy] = vector<Point>();
-            }
-            groupedByX[xx].push_back(Point{xx,yy});
-            groupedByY[yy].push_back(Point{xx,yy});
-            pts.insert(xx*(40001) + yy);
+            x2y[p[0]].insert(p[1]);
         }
-        int ans = INF;
-        for(auto g : groupedByX) {
-            auto group = g.second;
-            for(int i = 0; i < group.size() - 1; ++i) {
-                for(int j = i+1; j < group.size(); ++j) {
-                    int h = abs(group[i].y - group[j].y);
-                    for(auto p : groupedByY[group[i].y]) {
-                        if(p.x == group[i].x && p.y == group[i].y) continue;
-                        int w = max(abs(p.x - group[i].x), abs(p.x - group[j].x));
-                        Point rest = restPoint(group[i], group[j], p);
-                        if(pts.find(rest.x * 40001 + rest.y) != pts.end()) {
-                            ans = min(ans, w * h);
-                        }
-                    }
-                }
+        int N = points.size();
+        int ans = INT_MAX;
+        for(int i = 0; i < N-1; ++i) {
+            for(int j = i+1; j < N; ++j) {
+                if(points[i][0] == points[j][0] ||
+                  points[i][1] == points[j][1]) continue;
+                if(x2y[points[i][0]].find(points[j][1]) == x2y[points[i][0]].end()) continue;
+                if(x2y[points[j][0]].find(points[i][1]) == x2y[points[j][0]].end()) continue;
+                int w = abs(points[i][0] - points[j][0]);
+                int h = abs(points[i][1] - points[j][1]);
+                ans = min(ans, w * h);
             }
         }
-        return ans == INF ? 0 : ans;
+        return ans == INT_MAX ? 0 : ans;
     }
 };

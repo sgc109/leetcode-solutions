fad56da7a4baa17016776c0bc3c724d0d42caaf6
Sungho Hwang <sam.hwang@krustuniverse.com>
2022-03-29 15:56:09 +0900

    Time: 192 ms (88.10%), Space: 108.4 MB (68.87%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -11,22 +11,17 @@ public:
   vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
     int N = nums1.size();
     int M = nums2.size();
-    vector<int> shortList = N < M ? nums1 : nums2;
-    vector<int> longList = N < M ? nums2 : nums1;
     priority_queue<Node> pq;
-    for(int i = 0; i < min(k, (int)shortList.size()); ++i) {
-      pq.push(Node{shortList[i] + longList[0], i, 0});
+    for(int i = 0; i < min(k, N); ++i) {
+      pq.push(Node{nums1[i] + nums2[0], i, 0});
     }
     vector<vector<int>> ret;
     while(ret.size() < k && !pq.empty()) {
       Node cur = pq.top();
       pq.pop();
-      int ansFirst = shortList[cur.i];
-      int ansSecond = longList[cur.j];
-      if(N >= M) swap(ansFirst, ansSecond);
-      ret.push_back(vector<int>({ansFirst, ansSecond}));
-      if(cur.j + 1 < min(k, (int)longList.size())) {
-        pq.push(Node{shortList[cur.i] + longList[cur.j + 1], cur.i, cur.j + 1});
+      ret.push_back(vector<int>({nums1[cur.i], nums2[cur.j]}));
+      if(cur.j + 1 < min(k, M)) {
+        pq.push(Node{nums1[cur.i] + nums2[cur.j + 1], cur.i, cur.j + 1});
       }
     }
 

18400b8a844dfa1546b16e42cc56e6180300333c
Sungho Hwang <sam.hwang@krustuniverse.com>
2022-03-30 23:22:51 +0900

    Time: 267 ms (32.13%), Space: 53.3 MB (18.96%) - LeetHub

--- 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.cpp
+++ 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-  int maxSubArrayLen(vector<int>& nums, int k) { // nums = [1,-1,5,-2,3], k = 3
+  int maxSubArrayLen(vector<int>& nums, int k) {
     int N = nums.size();
-    unordered_map<int,int> minIdx; // {0=-1, 1=0, 3=3, 5=2, 6=4}
+    unordered_map<int,int> minIdx;
     minIdx[0] = -1;
-    vector<int> psum(N, 0); // psum = [1, 0, 5, 3, 6]
-    int ans = 0; // 4
+    vector<int> psum(N, 0);
+    int ans = 0;
     psum[0] = nums[0];
     for(int i = 1; i < N; ++i) {
       psum[i] = psum[i-1] + nums[i];
     }
-    for(int i = 0; i < N; ++i) { // i = 4
+    for(int i = 0; i < N; ++i) {
       if (minIdx.count(psum[i] - k) > 0) {
         int len = i - minIdx[psum[i] - k];
         ans = max(ans, len);

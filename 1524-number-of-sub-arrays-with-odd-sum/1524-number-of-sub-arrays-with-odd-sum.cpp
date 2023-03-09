01b001b45c6a838da6f924ea9ec0e21665c4c21e
Wood Hwang <sh.hwang@bucketplace.net>
2023-03-10 01:37:05 +0900

    Time: 416 ms (5.03%), Space: 182.3 MB (5.03%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -13,12 +13,12 @@ public:
         dp[0][0] = 0;
         dp[0][1] = 0;
         for(int i = 0; i < N; ++i) {
-            if(arr[i] % 2 == 0) {
-                dp[i + 1][0] = dp[i][0] + 1;
-                dp[i + 1][1] = dp[i][1];
-            } else {
+            if(arr[i] & 1) {
                 dp[i + 1][0] = dp[i][1];
                 dp[i + 1][1] = dp[i][0] + 1;
+            } else {
+                dp[i + 1][0] = dp[i][0] + 1;
+                dp[i + 1][1] = dp[i][1];
             }
         }
         

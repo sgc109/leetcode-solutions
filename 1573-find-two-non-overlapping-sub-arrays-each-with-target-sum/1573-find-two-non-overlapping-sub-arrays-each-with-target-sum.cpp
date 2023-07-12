c668c3dce300d612cedf4008e3fd20039148d16f
Wood Hwang <sh.hwang@bucketplace.net>
2023-07-13 03:10:15 +0900

    Time: 591 ms (29.92%), Space: 134.7 MB (41.24%) - LeetHub

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/1573-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,37 +1,30 @@
 class Solution {
-    int length[100003];
-
+    int minLen[100003];
 public:
     int minSumOfLengths(vector<int>& arr, int target) {
-        memset(length, 0x3c, sizeof(length));
+        memset(minLen, 0x3c, sizeof(minLen));
+        int acc = 0;
 
         int N = arr.size();
+        unordered_map<int, int> um;
         int ans = N + 1;
-        int sum = 0;
-        int r = 0;
-        int prevMinLen = N + 1;
-        for(int l = 0; l < N; ++l) {
-            while(r < N && sum < target) {
-                sum += arr[r];
-                ++r;
+        um[0] = -1;
+        for(int i = 0; i < N; ++i) {
+            if(i > 0) {
+                minLen[i] = minLen[i-1];
             }
-            if(sum == target) {
-                ans = min(ans, prevMinLen + r - l);
-                length[r - 1] = r - l;
+            acc += arr[i];
+            if(um.count(acc - target) > 0) {
+                printf("i: %d\n", i);
+                int len = i - um[acc - target];
+                if(um[acc - target] > -1) {
+                    ans = min(ans, minLen[um[acc - target]] + len);
+                }
+                minLen[i] = min(minLen[i], len);
             }
-            prevMinLen = min(prevMinLen, length[l]);
-            sum -= arr[l];
+            um[acc] = i;
         }
 
         return ans == N + 1 ? -1 : ans;
     }
 };
-
-
-/*
-
-a + x
-a + 2x
-
-
-*/

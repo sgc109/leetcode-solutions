11ba8181c0d7c3fd93fda6603bbfd3a7d9745509
Wood Hwang <sh.hwang@bucketplace.net>
2023-12-12 15:08:43 +0900

    Time: 355 ms (45.87%), Space: 81.9 MB (90.37%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        int ans = 0;
         unordered_map<int, int> counter;
-        
+        int ans = 0;
         for(int i = 0; i < nums.size(); ++i) {
             for(int j = i + 1; j < nums.size(); ++j) {
-                int cnt = counter[nums[i] * nums[j]];
-                ans += cnt * 8;
-                counter[nums[i] * nums[j]]++;
+                int multiple = nums[i] * nums[j];
+                ans += counter[multiple];
+                counter[multiple]++;
             }
         }
-              
-        return ans;
+        return ans * 8;
     }
 };

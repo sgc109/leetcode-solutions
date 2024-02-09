b04afd5383575d4f73df5494626330650f4fc7ea
Wood Hwang <sh.hwang@bucketplace.net>
2024-02-10 01:47:10 +0900

    Time: 16 ms (94.65%), Space: 27.3 MB (12.39%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int N = nums.size();
+        vector<int> stk;
+        vector<int> ans(N);
+        for (int i = N - 1; i >= 0; --i) {
+            stk.push_back(nums[i]);
+        }
+        for (int i = N - 1; i >= 0; --i) {
+            while (stk.size() > 0 && stk.back() <= nums[i]) {
+                stk.pop_back();
+            }
+            if (stk.size() == 0) {
+                ans[i] = -1;
+            } else {
+                ans[i] = stk.back();
+            }
+            stk.push_back(nums[i]);
+        }
+
+        return ans;
+    }
+};

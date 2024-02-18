ae6f17d5176a9ae99cbe4ebf3adc71a7edaaac1d
Wood Hwang <sh.hwang@bucketplace.net>
2024-02-18 23:24:58 +0900

    Time: 158 ms (49.91%), Space: 150.5 MB (5.06%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -2,11 +2,19 @@ class Solution {
 public:
     int maximumGap(vector<int>& nums) {
         long long base = 1;
+        
         while(base <= 1e9) {
             vector<vector<int>> buckets = vector<vector<int>>(10, vector<int>());
+            int cntLessThanBase = 0;
             for(int num : nums) {
+                if(num < base) {
+                    ++cntLessThanBase;
+                }
                 int digit = num / base % 10;
                 buckets[digit].push_back(num);
+            }
+            if(cntLessThanBase == size(nums)) {
+                break;
             }
             int pos = 0;
             for(int i = 0; i < 10; ++i) {

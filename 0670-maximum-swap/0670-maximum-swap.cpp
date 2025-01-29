1638232599a2e4c0e993a22ef123907290420655
Wood Hwang <sh.hwang@bucketplace.net>
2025-01-29 20:20:40 +0900

    Time: 0 ms (100%), Space: 8.3 MB (10.85%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        if(num < 10) {
-            return num;
+        vector<vector<int>> digitToIndexes(10, vector<int>());
+        string numStr = to_string(num);
+        for(int i  = size(numStr) - 1; i >=0 ; --i) {
+            digitToIndexes[numStr[i] - '0'].push_back(i);
         }
+        
+        int maxDigit = 9;
+        for(int i = 0; i < size(numStr); ++i) {
+            int digit = numStr[i] - '0';
 
-        int cntDigits = 0;
-        int curNum = num;
-        while(curNum) {
-            curNum /= 10;
-            cntDigits++;
-        }
-
-        int ans = num;
-        for(int i = 0; i < cntDigits; ++i) {
-            for(int j = i + 1; j < cntDigits; ++j) {
-                int tmp = num;
-
-                int digit1 = num / (int)pow(10, i) % 10;
-                int digit2 = num / (int)pow(10, j) % 10;
-
-                tmp -= digit1 * pow(10, i);
-                tmp -= digit2 * pow(10, j);
-                tmp += digit1 * pow(10, j);
-                tmp += digit2 * pow(10, i);
-
-                ans = max(ans, tmp);
+            while(digitToIndexes[maxDigit].size() == 0) {
+                maxDigit--;
             }
+
+            if(digit < maxDigit) {
+                swap(numStr[i], numStr[digitToIndexes[maxDigit][0]]);
+                return stoi(numStr);
+            }
+            
+            digitToIndexes[digit].pop_back();
         }
 
-        return ans;
+        return num;
     }
 };

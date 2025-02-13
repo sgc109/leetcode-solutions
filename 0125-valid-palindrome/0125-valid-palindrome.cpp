b5794d782f26129493d2285ffc6173bde45dbec0
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-13 22:17:41 +0900

    Time: 4 ms (17.87%), Space: 9.8 MB (79.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,4 @@
 class Solution {
-    bool isLower(char c) {
-        return 'a' <= c && c <= 'z';
-    }
-
-    bool isUpper(char c) {
-        return 'A' <= c && c <= 'Z';
-    }
-
-    bool isNumeric(char c) {
-        return '0' <= c && c <= '9';
-    }
-
-    bool isAlphaNumeric(char c) {
-        return isLower(c) || isUpper(c) || isNumeric(c);
-    }
-
-    char toLower(char c) {
-        if(isUpper(c)) {
-            return (char)(c - 'A' + 'a');
-        }
-        return c;
-    }
 public:
     // "A man, a plan, a canal: Panama"
     //                   ^            
@@ -28,16 +6,16 @@ public:
         int l = 0; // 0
         int r = s.size() - 1; // 29
         while(l < r) {
-            while(l < r && !isAlphaNumeric(s[l])) {
+            while(l < r && !isalnum(s[l])) {
                 ++l;   
             }
-            while(l < r && !isAlphaNumeric(s[r])) {
+            while(l < r && !isalnum(s[r])) {
                 --r;
             } 
             if(l == r) {
                 return true;
             }
-            if(toLower(s[l]) != toLower(s[r])) {
+            if(tolower(s[l]) != tolower(s[r])) {
                 return false;
             }
             ++l;

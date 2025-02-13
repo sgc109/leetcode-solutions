27124deef001764cf8b98b68132c19ad56bf3cab
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-13 22:16:08 +0900

    Time: 0 ms (100%), Space: 9.7 MB (93.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -10,22 +10,40 @@ class Solution {
     bool isNumeric(char c) {
         return '0' <= c && c <= '9';
     }
-public:
-    bool isPalindrome(string s) { // " "
-        string newS = ""; // ""
-        for(char c : s) { // ' '
-            if(isLower(c) || isNumeric(c)) {
-                newS += c;
-            } else if(isUpper(c)) {
-                newS += (char)(c - 'A' + 'a');
-            }
+
+    bool isAlphaNumeric(char c) {
+        return isLower(c) || isUpper(c) || isNumeric(c);
+    }
+
+    char toLower(char c) {
+        if(isUpper(c)) {
+            return (char)(c - 'A' + 'a');
         }
-        int N = newS.size(); // 0
-        for(int i = 0; i < N / 2; ++i) {
-            if(newS[i] != newS[N - i - 1]) {
+        return c;
+    }
+public:
+    // "A man, a plan, a canal: Panama"
+    //                   ^            
+    bool isPalindrome(string s) {
+        int l = 0; // 0
+        int r = s.size() - 1; // 29
+        while(l < r) {
+            while(l < r && !isAlphaNumeric(s[l])) {
+                ++l;   
+            }
+            while(l < r && !isAlphaNumeric(s[r])) {
+                --r;
+            } 
+            if(l == r) {
+                return true;
+            }
+            if(toLower(s[l]) != toLower(s[r])) {
                 return false;
             }
+            ++l;
+            --r;
         }
+        
         return true;
     }
 };

e1d3ff25bf299f204b6349c5452758cfdd4e4d1a
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-15 16:30:31 +0900

    Time: 1 ms (95.58%), Space: 24.9 MB (15.55%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,33 @@
 class Solution {
+    int starts[10003]; // [0, 1, 0, 0, 0, 0]
+    int ends[10003]; // [0, -1, 0, 0, 0, 0]
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if(intervals.size() == 0) {
-            return intervals;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) { // [[1,1]]
+        int minNum = 10003; // 1
+        int maxNum = -1; // 1
+        for(auto inter : intervals) {
+            starts[inter[0]]++;
+            ends[inter[1]]--;
+            minNum = min(minNum, inter[0]);
+            maxNum = max(maxNum, inter[1]);
         }
-        sort(begin(intervals), end(intervals));
-        vector<vector<int>> ret;
         
-        int s = intervals[0][0];
-        int e = intervals[0][1];
-        for(auto p : intervals) {
-            if(e < p[0]) {
-                ret.push_back(vector<int>({s, e}));
-                s = p[0];
-                e = p[1];
-            } else {
-                e = max(e, p[1]);
+        // starts = [0, 1, 0, 0, 0, 0]
+        // ends = [0, -1, 0, 0, 0, 0]
+        vector<vector<int>> ans; // [[1,1]]
+        int acc = 0; // 0
+        int start; // 1
+        for(int i = minNum; i <= maxNum; ++i) { // i = 1
+            if(starts[i] > 0 && acc == 0) {
+                start = i;
             }
+            acc += starts[i];
+
+            if(ends[i] < 0 && ends[i] + acc == 0) {
+                ans.push_back({start, i});
+            }
+            acc += ends[i];
         }
-        ret.push_back(vector<int>({s, e}));
-        
-        return ret;
+        return ans;
     }
 };

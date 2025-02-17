7d178e183cd806d4358e0a1d1c18d5212bdb719f
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-17 21:20:35 +0900

    Time: 10 ms (77.37%), Space: 18.6 MB (10%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -8,9 +8,11 @@
  * };
  */
 class Solution {
-    vector<TreeNode*> rootToP;
-    vector<TreeNode*> rootToQ;
-    TreeNode *p, *q;
+    vector<TreeNode*> rootToP; // [3, 5]
+    vector<TreeNode*> rootToQ; // [3, 1]
+    TreeNode *p, *q; // 5, 1
+     // cur = 3, 1
+     // fromRoot = [3, 1]
     void dfs(TreeNode* cur, vector<TreeNode*>& fromRoot) {
         if(!cur) {
             return;
@@ -22,20 +24,24 @@ class Solution {
             rootToQ = fromRoot;
         }
 
+        if(rootToP.size() > 0 && rootToQ.size() > 0) {
+            return;
+        }
+
         dfs(cur->left, fromRoot);
         dfs(cur->right, fromRoot);
 
         fromRoot.pop_back();
     }
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) { // 3, 5, 1
         this->p = p;
         this->q = q;
         
         vector<TreeNode*> fromRoot;
         dfs(root, fromRoot);
         int i;
-        for(i = 0; i < min(size(rootToP), size(rootToQ)); ++i) {
+        for(i = 0; i < min(size(rootToP), size(rootToQ)); ++i) { // i = 1
             if(rootToP[i]->val != rootToQ[i]->val) {
                 break;
             }

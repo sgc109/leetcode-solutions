ae8a0669a4fa428bfa1a23e57a113fe4a7afdd11
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-20 00:34:45 +0900

    Time: 0 ms (100%), Space: 8.9 MB (95.5%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        reverse(begin(num1), end(num1));
-        reverse(begin(num2), end(num2));
-        int pos = 0;
+        int p1 = num1.size() - 1;
+        int p2 = num2.size() - 1;
         int carry = 0;
-        string concat = "";
-        while(pos < num1.size() && pos < num2.size()) {
-            int n1 = num1[pos] - '0';
-            int n2 = num2[pos] - '0';
-            int res = n1 + n2 + carry;
-            concat += '0' + (res % 10);
-            carry = res > 9 ? 1 : 0;
-            ++pos;
+        string ans = "";
+        while(p1 >= 0 || p2 >= 0) {
+            int n1 = p1 >= 0 ? num1[p1] - '0' : 0;
+            int n2 = p2 >= 0 ? num2[p2] - '0' : 0;
+            int sum = n1 + n2 + carry;
+            ans += '0' + (sum % 10);
+            carry = sum >= 10 ? 1 : 0;
+            --p1;
+            --p2;
         }
-        while(pos < num1.size()) {
-            int n1 = num1[pos] - '0';
-            int res = n1 + carry;
-            concat += '0' + (res % 10);
-            carry = res > 9 ? 1 : 0;
-            ++pos;
-        }
-
-        while(pos < num2.size()) {
-            int n2 = num2[pos] - '0';
-            int res = n2 + carry;
-            concat += '0' + (res % 10);
-            carry = res > 9 ? 1 : 0;
-            ++pos;
-        }
-
         if(carry) {
-            concat += '1';
+            ans += '1';
         }
-
-        reverse(begin(concat), end(concat));
-        
-        return concat;
+        reverse(begin(ans), end(ans));
+        return ans;
     }
 };

f7164519e98da47fbfe74781d4fbafae6b6845db
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-19 22:54:47 +0900

    Time: 0 ms (100%), Space: 20.9 MB (88.84%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,35 +1,12 @@
 class Solution {
-    int R, C;
-    bool inRange(int r, int c) {
-        return 0 <= r && r < R && 0 <= c && c < C;
-    }
-    bool isPoss(int sr, int sc, vector<vector<int>>& matrix) {
-        int cr = sr;
-        int cc = sc;
-        int val = -1;
-        while(inRange(cr, cc)) {
-            if(val == -1) {
-                val = matrix[cr][cc];
-            } else if(val != matrix[cr][cc]) {
-                return false;
-            }
-            cr++;
-            cc++;
-        }
-        return true;
-    }
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        R = matrix.size();
-        C = matrix[0].size();
-        for(int i = 0; i < C; ++i) {
-            if(!isPoss(0, i, matrix)) {
-                return false;
-            }
-        }
-        for(int i = 1; i < R; ++i) {
-            if(!isPoss(i, 0, matrix)) {
-                return false;
+        int R = matrix.size(), C = matrix[0].size();
+        for(int i = 0; i < R; ++i) {
+            for(int j = 0; j < C; ++j) {
+                if(i > 0 && j > 0 && matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
             }
         }
         return true;

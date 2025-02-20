cacaa133f7cd5dba3a21f9777c1bc7e4b4175e46
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-21 00:48:09 +0900

    Time: 2 ms (1.28%), Space: 12.3 MB (91.25%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -12,13 +12,7 @@ public:
         int lo = 0, hi = nums.size() - 1;
         while(lo < hi) {
             int mid = (lo + hi) / 2;
-            int l = (mid == 0 || nums[mid - 1] < nums[mid]) ? -1 : 1;
-            int r = (mid == nums.size() - 1 || nums[mid] < nums[mid + 1]) ? 1 : -1;
-            if(l == -1 && r == -1) {
-                return mid;
-            } else if(l == 1 && r == 1) {
-                lo = mid + 1;
-            } else if(l == -1 && r == 1) {
+            if(nums[mid] < nums[mid + 1]) {
                 lo = mid + 1;
             } else {
                 hi = mid;

cbbefcb6dc042700dbc2205969c19136b83d2fad
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-20 21:55:52 +0900

    Time: 11 ms (89.88%), Space: 24.4 MB (70.92%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -7,35 +7,71 @@ class Solution {
         return 0 <= r && r < N && 0 <= c && c < N;
     }
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+/*
+0 1
+1 0
+OK
+
+0 0 0
+1 1 0
+1 1 0
+
+1 0 0
+1 1 0
+1 1 0
+
+0 0 0 0
+1 1 0 1
+1 0 1 1
+0 0 0 0
+
+1 2 3 4
+-1 -1 3 -1
+-1 4 -1 -1
+5 5 5 6
+
+[(3, 1), (3, 0)]
+
+0
+
+1
+
+grid:
+0
+
+dist:
+-1
+
+q: []
+*/
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) { // 
         N = grid.size();
-        vector<vector<int>> dist = vector<vector<int>>(N, vector<int>(N, -1));
         if(grid[0][0]) {
             return -1;
         }
         if(N == 1) {
             return 1;
         }
-        dist[0][0] = 1;
-        queue<pair<int, int>> q;
-        q.push({0, 0});
+        queue<pair<pair<int, int>, int>> q;
+        q.push({{0, 0}, 1});
         while(!q.empty()) {
             auto cur = q.front();
             q.pop();
-            int r = cur.first;
-            int c = cur.second;
+            int r = cur.first.first; // 0
+            int c = cur.first.second; // 1
+            int dist = cur.second;
 
             for(int i = 0; i < 8; ++i) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if(!inRange(nr, nc) || grid[nr][nc] || dist[nr][nc] != -1) {
+                int nr = r + dr[i]; // 1
+                int nc = c + dc[i]; // 2
+                if(!inRange(nr, nc) || grid[nr][nc]) {
                     continue;
                 }
-                dist[nr][nc] = dist[r][c] + 1;
                 if(nr == N - 1 && nc == N - 1) {
-                    return dist[nr][nc];
+                    return dist + 1;
                 }
-                q.push({nr, nc});
+                grid[nr][nc] = 1;
+                q.push({{nr, nc}, dist + 1});
             }
         }
         return -1;

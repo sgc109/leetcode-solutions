c202504e5975f15f3236d7db5b36df3fa1a7a03f
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-22 21:05:44 +0900

    Time: 7 ms (36.62%), Space: 22.1 MB (82.53%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -4,10 +4,13 @@ bcaacbc
 */
 
 class Solution {
-    bool checkPalin(string& s, int l, int r) { // s = bcaacbc, l = 3, r = 4
-        while(l < r) {
-            if(s[l] != s[r]) {
-                return false;
+    bool isPalin(string &s, int l, int r) { // l = 2, r = 4
+        if (l >= r) {
+            return true;
+        }
+        while (l < r) {
+            if (s[l] != s[r]) {
+            return false;
             }
             ++l;
             --r;
@@ -15,15 +18,16 @@ class Solution {
         return true;
     }
 public:
-    bool validPalindrome(string s) { // bcaacbc
-        int l = 0; // 0
-        int r = s.size() - 1; // 6
-        while(l < r) {
-            if(s[l] != s[r]) { // b != c
-                return checkPalin(s, l + 1, r) || checkPalin(s, l, r - 1);
-            }
+    bool validPalindrome(string s) { // s = "abcbda"
+        int l = 0;                         // 0
+        int r = s.size() - 1;              // 1
+        while (l < r) {
+            if (s[l] == s[r]) {
             ++l;
             --r;
+            } else {
+            return isPalin(s, l + 1, r) || isPalin(s, l, r - 1);
+            }
         }
         return true;
     }

577bb8b7a08fbe2f2fd5ac87b1aca1f803c7d758
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-22 21:03:11 +0900

    Time: 0 ms (100%), Space: 7.9 MB (82.48%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.cpp
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.cpp
@@ -1,47 +1,32 @@
 class Solution {
-    bool isNum(char c) {
-        return '0' <= c && c <= '9';
-    }
-
-    pair<int, int> parsePrefixNum(string& str, int pos) {
-        assert(isNum(str[pos]));
-        int i;
-        for(i = pos; i < str.size(); ++i) {
-            if(!isNum(str[i])) {
-                break;
-            }
-        }
-        int num = stoi(str.substr(pos, i - pos));
-        return {num, i};
-    }
-
-    bool checkIsValid(string& word, int pos1, string& abbr, int pos2) {
-        if(pos1 == word.size() && pos2 == abbr.size()) {
-            return true;
-        }
-        if(pos1 == word.size() || pos2 == abbr.size()) {
-            return false;
-        }
-        if(abbr[pos2] == '0') {
-            return false;
-        }
-        if(!isNum(abbr[pos2])) {
-            if(word[pos1] != abbr[pos2]) {
-                return false;
-            }
-            return checkIsValid(word, pos1 + 1, abbr, pos2 + 1);
-        } else {
-            auto result = parsePrefixNum(abbr, pos2);
-            int num = result.first;
-            int nextIdx = result.second;
-            if(word.size() - pos1 < num) {
-                return false;
-            }
-            return checkIsValid(word, pos1 + num, abbr, nextIdx);
-        }
-    }
 public:
-    bool validWordAbbreviation(string word, string abbr) {
-        return checkIsValid(word, 0, abbr, 0);
+    bool validWordAbbreviation(string word, string abbr) { // word = "substitution", abbr = "s010n",
+        int p1 = 0;                       // 1
+        int p2 = 0;                       // 1
+        int curNum = 0;                   // 0
+        while (p1 < word.size() && p2 < abbr.size()) { // 12, 2
+            if (isalpha(abbr[p2])) {
+                if (word[p1] != abbr[p2]) {
+                    return false;
+                } else {
+                    ++p1;
+                    ++p2;
+                }
+                } else {
+                if (curNum == 0 && abbr[p2] == '0') {
+                    return false;
+                }
+                while (p2 < abbr.size() && isdigit(abbr[p2])) {
+                    curNum = curNum * 10 + abbr[p2] - '0';
+                    ++p2;
+                }
+                if (word.size() - p1 < curNum) {
+                    return false;
+                }
+                p1 += curNum;
+                curNum = 0;
+            }
+        }
+        return p1 == word.size() && p2 == abbr.size();
     }
 };

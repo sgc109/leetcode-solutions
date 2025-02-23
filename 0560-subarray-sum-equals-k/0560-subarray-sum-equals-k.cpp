9cf2b3796ba4f689f08182590a662efef59fbf09
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-24 00:18:09 +0900

    Time: 54 ms (51.7%), Space: 51 MB (24.8%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,26 +1,15 @@
-/*
-
-[[1 2 3] 4 5] 6 
-
-cur_sum - prev_sum = k
-cur_sum - k = prev_sum
-
-*/
-
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> map;
-        map[0] = 1;
-
-        int ans = 0;
+        unordered_map<int, int> psumCounter;
+        psumCounter[0] = 1;
         int acc = 0;
-        for(int i = 0; i < size(nums); ++i) {
-            acc += nums[i];
-            ans += map[acc - k];
-            map[acc]++;
+        int ans = 0;
+        for(int num : nums) {
+            acc += num;
+            ans += psumCounter[acc - k];
+            psumCounter[acc]++;
         }
-
         return ans;
     }
 };

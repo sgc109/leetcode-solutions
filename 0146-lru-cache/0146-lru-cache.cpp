747c624ff2e35cb3b7c5bfe69582baeebc3303c9
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-23 12:59:37 +0900

    Time: 61 ms (87.06%), Space: 173.1 MB (79.86%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,69 +1,72 @@
 class LRUCache {
-    struct ListNode {
+    struct Node {
         int key;
         int val;
-        ListNode* left;
-        ListNode* right;
-        ListNode(int key, int val): key(key), val(val), left(nullptr), right(nullptr) {}
+        Node* prev;
+        Node* next;
+
+        Node(int key, int val): key(key), val(val), prev(nullptr), next(nullptr) {}
     };
-    unordered_map<int, ListNode*> keyToNode;
-    ListNode* head;
-    ListNode* tail;
-    int capacity;
 
-    void remove(ListNode* target) {
-        target->left->right = target->right;
-        target->right->left = target->left;
+    unordered_map<int, Node*> keyToNode;
+    int capacity; // 2
+    Node* head;
+    Node* tail;
+
+    void remove(Node* target) {
+        target->prev->next = target->next;
+        target->next->prev = target->prev;
+    }
+    
+    void append(Node* target) {
+        tail->prev->next = target;
+        target->prev = tail->prev;
+        target->next = tail;
+        tail->prev = target;
     }
 
-    void append(ListNode* target) {
-        tail->left->right = target;
-        target->left = tail->left;
-        target->right = tail;
-        tail->left = target;
-    }
-
-    void refresh(ListNode* target) {
+    void refresh(Node* target) {
         remove(target);
         append(target);
     }
-public:
-    LRUCache(int capacity) : capacity(capacity) {
-        head = new ListNode(0, 0);
-        tail = new ListNode(0, 0);
-        head->right = tail;
-        tail->left = head;
+
+    void evict() {
+        keyToNode.erase(head->next->key);
+        Node* backup = head->next;
+        head->next->next->prev = head;
+        head->next = head->next->next;
+        delete backup;
     }
-    
+
+public:
+    LRUCache(int capacity): capacity(capacity) {
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head->next = tail;
+        tail->prev = head;
+    }
+
     int get(int key) {
         if(!keyToNode.count(key)) {
             return -1;
         }
-        auto node = keyToNode[key];
-        refresh(node);
-        return node->val;
+        Node* cur = keyToNode[key];
+        refresh(cur);
+        return cur->val;
     }
-    
-    void put(int key, int value) {
-        if(!keyToNode.count(key)) {
-            if(keyToNode.size() == capacity) {
-                keyToNode.erase(head->right->key);
-                remove(head->right);
-            }
-            ListNode* newNode = new ListNode(key, value);
-            keyToNode[key] = newNode;
-            append(newNode);
+
+    void put(int key, int value) { // 3, 3
+        if(keyToNode.count(key)) {
+            Node* cur = keyToNode[key];
+            cur->val = value;
+            refresh(cur);
             return;
         }
-        ListNode* node = keyToNode[key];
-        node->val = value;
-        refresh(node);
+        if(keyToNode.size() == capacity) {
+            evict();
+        }
+        Node* newNode = new Node(key, value);
+        keyToNode[key] = newNode;
+        append(newNode);
     }
-};
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
+ };

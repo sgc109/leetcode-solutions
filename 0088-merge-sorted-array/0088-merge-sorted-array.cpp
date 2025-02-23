fad860f5cc2fcc1e22c0adb9a844913646622b87
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-23 13:22:31 +0900

    Time: 0 ms (100%), Space: 12.3 MB (39.61%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int pos1 = m - 1;
-        int pos2 = n - 1;
-        int pos3 = m + n - 1;
-
-        while(pos1 >= 0 && pos2 >= 0) {
-            if(nums1[pos1] <= nums2[pos2]) {
-                nums1[pos3--] = nums2[pos2--];
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) { // nums1 = [1], m = 0, nums2 = [1], n = 1
+    //                                                                               ^                         ^    
+        int ptr1 = m - 1; // -1
+        int ptr2 = n - 1; // 0
+        int ptr3 = m + n - 1; // 0
+        while(ptr1 >= 0 && ptr2 >= 0) {
+            if(nums1[ptr1] < nums2[ptr2]) {
+                nums1[ptr3--] = nums2[ptr2--];
             } else {
-                nums1[pos3--] = nums1[pos1--];
+                nums1[ptr3--] = nums1[ptr1--];
             }
         }
-
-        while(pos2 >= 0) {
-            nums1[pos3--] = nums2[pos2--];
+        while(ptr2 >= 0) {
+            nums1[ptr3--] = nums2[ptr2--];
         }
     }
 };

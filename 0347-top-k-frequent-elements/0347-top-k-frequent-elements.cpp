730fb300c5c9e8a11d3d2cb0a4b4a8378d522622
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-24 01:00:26 +0900

    Time: 0 ms (100%), Space: 19.8 MB (10.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -5,18 +5,22 @@ public:
         for(int num : nums) {
             counter[num]++;
         }
-        vector<vector<int>> buckets(nums.size() + 1, vector<int>());
+        
+        vector<vector<int>> buckets = vector<vector<int>>(nums.size() + 1, vector<int>());
         for(auto it : counter) {
             buckets[it.second].push_back(it.first);
         }
 
         vector<int> ans;
-        for(int i = nums.size(); i >= 1 && ans.size() < k; --i) {
-            for(int num : buckets[i]) {
-                ans.push_back(num);
-            }
+        for(int i = nums.size(); i >= 1; --i) {
             if(ans.size() == k) {
                 break;
+            }
+            for(int j = 0; j < buckets[i].size(); ++j) {
+                ans.push_back(buckets[i][j]);
+                if(ans.size() == k) {
+                    break;
+                }
             }
         }
         return ans;

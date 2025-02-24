d7ba10b43a02b3b3b387d70ba2e234c02e77ddcb
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-24 20:46:45 +0900

    Time: 3 ms (52.03%), Space: 23 MB (63.96%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -1,31 +1,32 @@
+/*
+[[1,2]]
+[[]]
+*/
+
 class Solution {
-    vector<int> getIntersection(vector<int> inter1, vector<int> inter2) {
-        if(inter1[0] > inter2[0]) {
-            swap(inter1, inter2);
-        }
-        if(inter1[1] >= inter2[0]) {
-            return vector<int>({inter2[0], min(inter1[1], inter2[1])});
-        }
-        return vector<int>({-1, -1});
-    }
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        int pos1 = 0;
-        int pos2 = 0;
-        
+        int p1 = 0;
+        int p2 = 0;
         vector<vector<int>> ans;
-        while(pos1 < size(firstList) && pos2 < size(secondList)) {
-            vector<int> intersect = getIntersection(firstList[pos1], secondList[pos2]);
-            if(intersect[0] != -1) {
-                ans.push_back(intersect);
-            }
-            if(firstList[pos1][1] < secondList[pos2][1]) {
-                ++pos1;
+        while(p1 < firstList.size() && p2 < secondList.size()) {
+            int start1 = firstList[p1][0];
+            int end1 = firstList[p1][1];
+            int start2 = secondList[p2][0];
+            int end2 = secondList[p2][1];
+
+            if(end1 <= end2) {
+                if(start2 <= end1) {
+                    ans.push_back({max(start1, start2), end1});
+                }
+                ++p1;
             } else {
-                ++pos2;
+                if(start1 <= end2) {
+                    ans.push_back({max(start1, start2), end2});
+                }
+                ++p2;
             }
         }
-        
         return ans;
     }
 };

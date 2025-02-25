4c04dae2e852dfbed48b518e180675a5f976461e
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-25 20:47:54 +0900

    Time: 0 ms (100%), Space: 9.3 MB (30.92%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.cpp
+++ 0163-missing-ranges/0163-missing-ranges.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    // nums = []
-    // lower = 1
-    // upper = 10
     vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
-        vector<vector<int>> ret; // [[1,10]]
-        if(nums.size() == 0) {
-            ret.push_back({lower, upper});
-            return ret;
+        if(nums.empty()) {
+            return {{lower, upper}};
         }
-        if(lower < nums[0]) {
-            ret.push_back({lower, nums[0] - 1});
-        }
-
-        int N = nums.size(); // 1
-        for(int i = 0; i < N - 1; ++i) { // i = 0
-            if(nums[i] < nums[i + 1] - 1) {
-                ret.push_back({nums[i] + 1, nums[i + 1] - 1});
+        int prev = lower - 1;
+        vector<vector<int>> ans;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(prev + 1 <= nums[i] - 1) {
+                ans.push_back({prev + 1, nums[i] - 1});
             }
+            prev = nums[i];
         }
-        if(nums.back() < upper) {
-            ret.push_back({nums.back() + 1, upper});
+        if(prev + 1 <= upper) {
+            ans.push_back({prev + 1, upper});
         }
-
-        return ret;
+        return ans;
     }
 };

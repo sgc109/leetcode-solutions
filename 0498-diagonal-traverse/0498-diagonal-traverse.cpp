4fad239ad08b8705b05a1adc8f7392721bcd2841
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-25 22:23:06 +0900

    Time: 0 ms (100%), Space: 22.8 MB (59.31%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,43 +1,60 @@
 class Solution {
-    int R, C;
+    int du[2][2] = {{0, 1}, {1, 0}};
+    int dd[2][2] = {{1, 0}, {0, 1}};
+    int R;
+    int C;
     bool inRange(int r, int c) {
         return 0 <= r && r < R && 0 <= c && c < C;
     }
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        R = mat.size();
+        R  = mat.size();
         C = mat[0].size();
-
+        bool isGoingUp = true;
+        int cr = 0;
+        int cc = 0;
         vector<int> ans;
-
-        int r = 0, c = 0;
-        int dr = -1, dc = 1;
-        while(r != R - 1 || c != C - 1) {
-            while(true) {
-                ans.push_back(mat[r][c]);
-                if(!inRange(r + dr, c + dc)) {
-                    break;
+        while(cr != R - 1 || cc != C - 1) {
+            ans.push_back(mat[cr][cc]);
+            if(isGoingUp) {
+                int nr = cr - 1;
+                int nc = cc + 1;
+                if(inRange(nr, nc)) {
+                    cr = nr;
+                    cc = nc;
+                    continue;
                 }
-                r += dr, c += dc;
+                isGoingUp = !isGoingUp;
+                nr = cr + du[0][0];
+                nc = cc + du[0][1];
+                if(inRange(nr, nc)) {
+                    cr = nr;
+                    cc = nc;
+                    continue;
+                }
+                cr += du[1][0];
+                cc += du[1][1];
+            } else {
+                int nr = cr + 1;
+                int nc = cc - 1;
+                if(inRange(nr, nc)) {
+                    cr = nr;
+                    cc = nc;
+                    continue;
+                }
+                isGoingUp = !isGoingUp;
+                nr = cr + dd[0][0];
+                nc = cc + dd[0][1];
+                if(inRange(nr, nc)) {
+                    cr = nr;
+                    cc = nc;
+                    continue;
+                }
+                cr += dd[1][0];
+                cc += dd[1][1];
             }
-            if(dr == -1) { // going up & right
-                if(inRange(r, c + 1)) { // try to go right
-                    c++;
-                } else {
-                    r++;
-                }
-            } else { // going down & left
-                if(inRange(r + 1, c)) { // try to go down
-                    r++;
-                } else {
-                    c++;
-                }
-            }
-            dr *= -1;
-            dc *= -1;
         }
         ans.push_back(mat[R - 1][C - 1]);
-
         return ans;
     }
 };

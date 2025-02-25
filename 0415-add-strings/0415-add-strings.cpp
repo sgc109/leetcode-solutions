f9ec80f507ce0593eb631c5cf7d3fe07f864e41e
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-25 20:32:18 +0900

    Time: 0 ms (100%), Space: 9.1 MB (81.62%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int p1 = num1.size() - 1;
-        int p2 = num2.size() - 1;
         int carry = 0;
         string ans = "";
+        int p1 = num1.size() - 1;
+        int p2 = num2.size() - 1;
         while(p1 >= 0 || p2 >= 0) {
-            int n1 = p1 >= 0 ? num1[p1] - '0' : 0;
-            int n2 = p2 >= 0 ? num2[p2] - '0' : 0;
-            int sum = n1 + n2 + carry;
-            ans += '0' + (sum % 10);
-            carry = sum >= 10 ? 1 : 0;
+            int digit1 = (p1 < 0 ? 0 : num1[p1] - '0');
+            int digit2 = (p2 < 0 ? 0 : num2[p2] - '0');
+            int sum = carry + digit1 + digit2;
+            ans += '0' + sum % 10;
+            carry = sum > 9;
             --p1;
             --p2;
         }
@@ -18,6 +18,6 @@ public:
             ans += '1';
         }
         reverse(begin(ans), end(ans));
-        return ans;
+        return ans == "" ? "0" : ans;
     }
 };

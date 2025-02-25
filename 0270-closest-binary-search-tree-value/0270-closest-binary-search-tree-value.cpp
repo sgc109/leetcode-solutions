ca9654289c5028f653cfe8c3690ee747fe60e9c3
Wood Hwang <sh.hwang@bucketplace.net>
2025-02-25 21:03:54 +0900

    Time: 0 ms (100%), Space: 21.1 MB (78.85%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
@@ -8,39 +8,26 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
-
-    5
-   / \
-  5   5
-
-
  */
 class Solution {
-    double minDiff = 2e9 + 1; // 3.428571
-    int minTarget; // 1
-    void dfs(TreeNode* cur, double target) { // cur = 1, target = 4.428571
-        if(!cur) {
-            return;
-        }
-        double diff = abs(target - cur->val); // 3.428571
-        if(diff == 0) {
-            minTarget = cur->val;
-            return;
-        }
-
-        if(diff < minDiff || (diff == minDiff && cur->val < minTarget)) {
+    void dfs(TreeNode* cur, double target, double& minDiff, int& ans) {
+        double diff = abs(cur->val - target);
+        if(minDiff > diff || (minDiff == diff && cur->val < ans)) {
+            ans = cur->val;
             minDiff = diff;
-            minTarget = cur->val;
         }
-        if(cur->val < target) {
-            dfs(cur->right, target);
-        } else {
-            dfs(cur->left, target);
+        if(cur->left && target <= cur->val) {
+            dfs(cur->left, target, minDiff, ans);
+        }
+        if(cur->right && cur->val <= target) {
+            dfs(cur->right, target, minDiff, ans);
         }
     }
 public:
-    int closestValue(TreeNode* root, double target) { // root = 1, target = 4.428571
-        dfs(root, target);
-        return minTarget;
+    int closestValue(TreeNode* root, double target) {
+        int ans;
+        double minDiff = INT_MAX;
+        dfs(root, target, minDiff, ans);
+        return ans;
     }
 };

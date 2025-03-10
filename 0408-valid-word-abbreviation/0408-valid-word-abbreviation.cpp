485af60315f82bbf1fc01d72672dde29733cf4ac
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-11 03:10:14 +0900

    Time: 0 ms (100%), Space: 7.9 MB (82.46%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.cpp
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    bool validWordAbbreviation(string word, string abbr) { // word = "substitution", abbr = "s010n",
-        int p1 = 0;                       // 1
-        int p2 = 0;                       // 1
-        int curNum = 0;                   // 0
-        while (p1 < word.size() && p2 < abbr.size()) { // 12, 2
-            if (isalpha(abbr[p2])) {
-                if (word[p1] != abbr[p2]) {
+/*
+        01234567890123456789
+word = "appe"
+            ^
+
+        0123456
+abbr = "a2ea"
+           ^
+*/
+    bool validWordAbbreviation(string word, string abbr) {
+        int p1 = 0; // 19
+        int p2 = 0; // 6
+        while(p1 < word.size() && p2 < abbr.size()) { // 20, 7
+            if(isalpha(abbr[p2])) {
+                if(word[p1] != abbr[p2]) {
                     return false;
-                } else {
-                    ++p1;
-                    ++p2;
                 }
-                } else {
-                if (curNum == 0 && abbr[p2] == '0') {
+                ++p1;
+                ++p2;
+            } else {
+                if(abbr[p2] == '0') {
                     return false;
                 }
-                while (p2 < abbr.size() && isdigit(abbr[p2])) {
+                int curNum = 0; // 2
+                while(p2 < abbr.size() && isdigit(abbr[p2])) {
                     curNum = curNum * 10 + abbr[p2] - '0';
                     ++p2;
                 }
-                if (word.size() - p1 < curNum) {
+                if(word.size() - p1 < curNum) {
                     return false;
                 }
                 p1 += curNum;
-                curNum = 0;
             }
         }
         return p1 == word.size() && p2 == abbr.size();

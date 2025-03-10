cd276d5c44b4affde1ff8dc883cb8e5281da4faa
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-10 22:28:50 +0900

    Time: 99 ms (99.87%), Space: 169.9 MB (92.16%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.cpp
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.cpp
@@ -1,30 +1,40 @@
+// nums1 = [0,1,0,0,2,0,0], nums2 = [1,0,0,0,3,0,4]
+
+template<typename T>
 class SparseVector {
-    unordered_map<int, int> map; // {0 -> 1, 3 -> 2, 4 -> 3} | {1 -> 3, 3 -> 4}
+    vector<pair<int,T> > indexAndVals;
 public:
-    SparseVector(vector<int> &nums) { // 1,0,0,2,3 | 0,3,0,4,0
+    SparseVector(vector<T> &nums) {
         for(int i = 0; i < nums.size(); ++i) {
             if(nums[i]) {
-                map[i] = nums[i];
+                indexAndVals.push_back({i, nums[i]});
             }
-        } 
-    }
-
-    int getNonZeroCount() {
-        return map.size();
-    }
-
-    int getValue(int index) {
-        return map[index];
-    }
-    
-    // Return the dotProduct of two sparse vectors
-    int dotProduct(SparseVector& vec) {
-        if(map.size() > vec.getNonZeroCount()) {
-            return vec.dotProduct(*this);
         }
-        int acc = 0;
-        for(auto it : map) {
-            acc += vec.getValue(it.first) * it.second;
+    }
+    /*
+     [[1,1],[4,2]]
+                   ^
+
+     [[0,1],[4,3],[6,4]]
+                    ^
+    */
+    T dotProduct(SparseVector& vec) { // 
+        int p1 = 0; // 0
+        int p2 = 0;
+        T acc = 0; // 6
+        while(p1 < indexAndVals.size() && p2 < vec.indexAndVals.size()) {
+            auto pair1 = indexAndVals[p1];
+            auto pair2 = vec.indexAndVals[p2];
+            
+            if(pair1.first == pair2.first) {
+                acc += pair1.second * pair2.second;
+                ++p1;
+                ++p2;
+            } else if(pair1.first < pair2.first) {
+                ++p1;
+            } else {
+                ++p2;
+            }
         }
         return acc;
     }

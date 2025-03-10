7546fb3109f466b75ca74bd4459541a0988098a3
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-11 01:13:02 +0900

    Time: 0 ms (100%), Space: 12.4 MB (39.26%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -10,12 +10,11 @@ nums2 = [1], n = 1
         --m; // 0
         --n; // -1
         while(n >= 0 || m >= 0) {
-            if(m == -1) {
-                nums1[m + n + 1] = nums2[n];
-                --n;
-            } else if(n == -1) {
+            if(n < 0) {
                 break;
-            } else if(nums1[m] < nums2[n]) {
+            }
+            
+            if(m < 0 || nums1[m] < nums2[n]) {
                 nums1[m + n + 1] = nums2[n];
                 --n;
             } else {

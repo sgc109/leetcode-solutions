3feb99f8fdb17169151c29d74ea55065fd9a8341
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-13 00:24:38 +0900

    Time: 9 ms (21.12%), Space: 15.6 MB (36%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -11,29 +11,52 @@ class Solution {
     unordered_map<TreeNode*, TreeNode*> parents;
     unordered_set<TreeNode*> visited;
     vector<int> ans;
-    void buildParents(TreeNode* cur, TreeNode* parent) {
-        if(cur) {
-            parents[cur] = parent;
-            buildParents(cur->left, cur);
-            buildParents(cur->right, cur);
+    void buildParents(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            TreeNode* cur = q.front();
+            q.pop();
+            if(cur->left) {
+                parents[cur->left] = cur;
+                q.push(cur->left);
+            }
+            if(cur->right) {
+                parents[cur->right] = cur;
+                q.push(cur->right);
+            }
         }
     }
-    void dfs(TreeNode* cur, int dist) {
-        if(!cur || visited.count(cur)) {
-            return;
+    void bfs(TreeNode* cur, int dist) {
+        queue<pair<TreeNode*,int>> q;
+        q.push({cur, dist});
+        while(!q.empty()) {
+            auto front = q.front();
+            q.pop();
+            TreeNode* cur = front.first;
+            int curDist = front.second;
+            if(visited.count(cur)) {
+                continue;
+            }
+            visited.insert(cur);
+            if(curDist == 0) {
+                ans.push_back(cur->val);
+            }
+            if(cur->left) {
+                q.push({cur->left, curDist - 1});
+            }
+            if(cur->right) {
+                q.push({cur->right, curDist - 1});
+            }
+            if(parents[cur]) {
+                q.push({parents[cur], curDist - 1});
+            }
         }
-        if(dist == 0) {
-            ans.push_back(cur->val);
-        }
-        visited.insert(cur);
-        dfs(cur->left, dist - 1);
-        dfs(cur->right, dist - 1);
-        dfs(parents[cur], dist - 1);
     }
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        buildParents(root, nullptr);
-        dfs(target, k);
+        buildParents(root);
+        bfs(target, k);
         return ans;
     }
 };

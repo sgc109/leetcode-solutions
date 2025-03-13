aaee3b837610c7654686853e0570c2876ef28b00
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-13 23:38:44 +0900

    Time: 18 ms (64.43%), Space: 25.4 MB (55%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,77 +1,48 @@
 class Solution {
-    int dr[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
-    int dc[8] = {0, 1, 1, 1, 0, -1, -1, -1};
-
-    int N;
+    int R;
+    int C;
     bool inRange(int r, int c) {
-        return 0 <= r && r < N && 0 <= c && c < N;
+        return 0 <= r && r < R && 0 <= c && c < C;
     }
-public:
-/*
+    /*
+    
+    8 1 2
+    7 0 3
+    6 5 4
+
 0 1
 1 0
-OK
 
-0 0 0
-1 1 0
-1 1 0
-
-1 0 0
-1 1 0
-1 1 0
-
-0 0 0 0
-1 1 0 1
-1 0 1 1
-0 0 0 0
-
-1 2 3 4
--1 -1 3 -1
--1 4 -1 -1
-5 5 5 6
-
-[(3, 1), (3, 0)]
-
-0
-
-1
-
-grid:
-0
-
-dist:
--1
-
-q: []
-*/
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) { // 
-        N = grid.size();
-        if(grid[0][0]) {
+    */
+    int dr[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+    int dc[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        this->R = grid.size(); // 2
+        this->C = grid[0].size(); // 2
+        if(grid[0][0] == 1 || grid.back().back() == 1) {
             return -1;
         }
-        if(N == 1) {
-            return 1;
-        }
-        queue<pair<int, int>> q;
+        vector<vector<int>> dist = vector<vector<int>>(R, vector<int>(C, -1));
+        queue<pair<int,int>> q; // []
         q.push({0, 0});
-        grid[0][0] = 1;
+        dist[0][0] = 1;
         while(!q.empty()) {
-            auto cur = q.front();
+            auto front = q.front();
             q.pop();
-            int r = cur.first; // 0
-            int c = cur.second; // 1
-
+            int curR = front.first;
+            int curC = front.second;
+            if(curR == R - 1 && curC == C - 1) {
+                return dist[curR][curC];
+            }
             for(int i = 0; i < 8; ++i) {
-                int nr = r + "00122210"[i] - '1'; // 1
-                int nc = c + "12221000"[i] - '1'; // 2
-                if(!inRange(nr, nc) || grid[nr][nc]) {
+                int nextR = curR + dr[i];
+                int nextC = curC + dc[i];
+                if(!inRange(nextR, nextC) || grid[nextR][nextC] || dist[nextR][nextC] != -1) {
                     continue;
                 }
-                if(nr == N - 1 && nc == N - 1) {
-                    return grid[r][c] + 1;
-                }
-                grid[nr][nc] = grid[r][c] + 1;
-                q.push({nr, nc});
+                dist[nextR][nextC] = dist[curR][curC] + 1;
+                q.push({nextR, nextC});
             }
         }
         return -1;

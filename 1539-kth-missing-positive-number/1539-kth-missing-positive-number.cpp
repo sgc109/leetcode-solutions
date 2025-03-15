8b0cde2c59dc9c978091566dbd5e315eaf5f622a
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-16 00:50:37 +0900

    Time: 0 ms (100%), Space: 13.2 MB (59.74%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int l = -1, r = arr.size();
-        while(l < r - 1) {
-            int m = (l + r) / 2;
-            if((m == -1 ? 0 : arr[m]) - (m + 1) < k) {
-                l = m;
-            } else {
-                r = m;
+        for(int i = 0; i < arr.size(); ++i) {
+            if(k + i < arr[i]) {
+                return k + i;
             }
         }
-        return k + l + 1;
+        return k + arr.size();
     }
 };

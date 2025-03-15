01b1b78316ed24e5f21b555c69356c869de45396
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-15 14:36:17 +0900

    Time: 88 ms (37.26%), Space: 76.3 MB (44.55%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,27 +1,22 @@
 class Solution {
-    struct Point {
-        int x;
-        int y;
-        bool operator<(const Point& rhs) const {
-            return x * x + y * y < rhs.x * rhs.x + rhs.y * rhs.y;
+    struct Comparator {
+        bool operator()(const vector<int>& a, const vector<int>& b) const {
+            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
         }
     };
 public:
-// points = [], k = 
-//                          ^
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<Point> pq; // [(3,3,18),(-2,4,20)]
+        priority_queue<vector<int>, vector<vector<int>>, Comparator> pq; // [(3,3,18),(-2,4,20)]
         for(auto& point : points) {
-            pq.push(Point{point[0], point[1]});
+            pq.push(point);
             if(pq.size() > k) {
                 pq.pop();
             }
         }
         vector<vector<int>> ans; // [(-2,2)]
         while(!pq.empty()) {
-            auto [x, y] = pq.top();
+            ans.push_back(pq.top());
             pq.pop();
-            ans.push_back({x, y});
         }
         return ans;
     }

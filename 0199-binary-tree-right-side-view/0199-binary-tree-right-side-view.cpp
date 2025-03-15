68b4586442714c597d7bbc5bfda4a559c1146f1b
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-15 14:16:44 +0900

    Time: 0 ms (100%), Space: 15.2 MB (26.87%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -8,34 +8,42 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
+
+
+      1
+    2   3
+     5   4
+
+        1
+      2   3
+    4 
+   5 
+
+     1
+       3
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
+    vector<int> rightSideView(TreeNode* root) { // 1
+        vector<int> ans; // []
         if(!root) {
-            return {};
+            return ans;
         }
-        vector<int> ans;
-        queue<pair<TreeNode*, int>> q;
+        queue<pair<TreeNode*,int>> q; // []
         q.push({root, 0});
-        int prevVal;
         while(!q.empty()) {
-            auto cur = q.front();
+            auto [cur, level] = q.front(); // (3,1)
             q.pop();
-            auto curNode = cur.first;
-            int curLevel = cur.second;
-            if(ans.size() == curLevel - 1) {
-                ans.push_back(prevVal);
+            if(ans.size() == level) {
+                ans.push_back(cur->val);
             }
-            prevVal = curNode->val;
-            if(curNode->left) {
-                q.push({curNode->left, curLevel + 1});
+            if(cur->right) {
+                q.push({cur->right, level + 1});
             }
-            if(curNode->right) {
-                q.push({curNode->right, curLevel + 1});
+            if(cur->left) {
+                q.push({cur->left, level + 1});
             }
         }
-        ans.push_back(prevVal);
         return ans;
     }
 };

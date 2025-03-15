616eb56a99893b4a2900d9f2444c94f3a7ab2cb8
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-15 23:06:41 +0900

    Time: 80 ms (37.19%), Space: 146.3 MB (70.04%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,40 @@
+/*
+23 2 4 6 7
+
+23 25 29 35 42
+5 1 5 5 0
+
+23 2 6 4 7
+23 25 31 35 42
+5 1 1 5 0
+
+23 2 6 4 7
+23 25 31 35 42
+10 12 5 9 3
+
+psum[i] = sum(nums[0]..nums[i])
+sum(nums[i]..nums[j]) = psum[j] - psum[i - 1]
+
+*/
+
+class Solution {
+// [23,2,4,6,7], k = 6
+//       ^
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> firstModIdx; // {0: -1, 5:0, 1:1, }
+        firstModIdx[0] = -1;
+        int acc = 0; // 29
+        for(int i = 0; i < nums.size(); ++i) {
+            acc += nums[i];
+            int mod = acc % k; // 5
+            if(firstModIdx.count(mod) && i - firstModIdx[mod] >= 2) {
+                return true;
+            }
+            if(!firstModIdx.count(mod)) {
+                firstModIdx[mod] = i;
+            }
+        }
+        return false;
+    }
+};

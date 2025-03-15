3730a47ad970011e827445b06a03498c4b799bfd
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-15 14:50:19 +0900

    Time: 23 ms (92.83%), Space: 61.5 MB (98.07%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,23 +1,10 @@
 class Solution {
-    struct Comparator {
-        bool operator()(const vector<int>& a, const vector<int>& b) const {
-            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
-        }
-    };
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<vector<int>, vector<vector<int>>, Comparator> pq; // [(3,3,18),(-2,4,20)]
-        for(auto& point : points) {
-            pq.push(point);
-            if(pq.size() > k) {
-                pq.pop();
-            }
-        }
-        vector<vector<int>> ans; // [(-2,2)]
-        while(!pq.empty()) {
-            ans.push_back(pq.top());
-            pq.pop();
-        }
-        return ans;
+        auto cmp = [](vector<int>& a, vector<int>& b) {
+            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
+        };
+        sort(points.begin(), points.end(), cmp);
+        return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };

a37279bc2d7747f23f7b7e00cb9c00e9dcecd11b
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-15 15:21:54 +0900

    Time: 23 ms (92.83%), Space: 64.8 MB (86.42%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,10 +1,44 @@
 class Solution {
+    typedef vector<int> Point;
+    function<bool(Point&, Point&)> cmp = [](Point& a, Point& b) {
+        return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
+    };
+        function<bool(Point&, Point&)> cmp2 = [](Point& a, Point& b) {
+        return a[0] * a[0] + a[1] * a[1] <= b[0] * b[0] + b[1] * b[1];
+    };
+    void quickSelect(int l, int r, int k, vector<Point>& points, vector<Point>& ans) { // 0, 1, 0, [[-2,2],[1,3]], [[-2,2]]
+        int randIdx = rand() % (r - l + 1) + l;
+        swap(points[randIdx], points[r]);
+        int p = l;
+        for(int i = l; i < r; ++i) {
+            int sameToTheRight = rand() % 2;
+            if((sameToTheRight && cmp(points[i], points[r]) ||
+               (!sameToTheRight && cmp2(points[i], points[r]))
+            )) {
+                swap(points[i], points[p]);
+                ++p;
+            }
+        }
+        swap(points[p], points[r]);
+        if(p == k) {
+            for(int i = l; i <= p; ++i) {
+                ans.push_back(points[i]);
+            }
+        } else if(p < k) {
+            for(int i = l; i <= p; ++i) {
+                ans.push_back(points[i]);
+            }
+            quickSelect(p + 1, r, k, points, ans);
+        } else {
+            quickSelect(l, p - 1, k, points, ans);
+        }
+    }
 public:
+// points = [[1,3],[-2,2]], k = 1
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        auto cmp = [](vector<int>& a, vector<int>& b) {
-            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
-        };
-        sort(points.begin(), points.end(), cmp);
-        return vector<vector<int>>(points.begin(), points.begin() + k);
+        --k;
+        vector<Point> ans;
+        quickSelect(0, points.size() - 1, k, points, ans);
+        return ans;
     }
 };

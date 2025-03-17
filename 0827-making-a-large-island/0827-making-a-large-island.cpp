bd4d342c93bf4e33767a15f9e23cccbc3a844019
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-17 23:26:43 +0900

    Time: 181 ms (74.66%), Space: 117 MB (66.51%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,89 +1,62 @@
-/*
-Example 1
-1 0
-0 1
-
-Example 2
-1 1
-1 0
-
-Example 3
-1 1
-1 1
-*/
 class Solution {
-    int N;
-    vector<int> parents;
-    vector<int> sizes;
     int dr[4] = {-1, 0, 1, 0};
     int dc[4] = {0, 1, 0, -1};
+    int N;
+    vector<vector<int>> labels;
+    vector<vector<int>> grid;
     bool inRange(int r, int c) {
         return 0 <= r && r < N && 0 <= c && c < N;
     }
-    int find(int u) {
-        if(parents[u] == u) {
-            return u;
+    int dfs(int r, int c, int label) {
+        int cnt = 1;
+        for(int i = 0; i < 4; ++i) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if(inRange(nr, nc) && grid[nr][nc] && !labels[nr][nc]) {
+                labels[nr][nc] = label;
+                cnt += dfs(nr, nc, label);
+            }
         }
-        return parents[u] = find(parents[u]);
-    }
-    int merge(int u, int v) {
-        u = find(u), v = find(v);
-        if(u == v) {
-            return sizes[u];
-        }
-        if(sizes[u] > sizes[v]) {
-            swap(u, v);
-        }
-        sizes[v] += sizes[u];
-        parents[u] = v;
-        return sizes[v];
+        return cnt;
     }
 public:
     int largestIsland(vector<vector<int>>& grid) {
-        N = grid.size();
-        sizes = vector<int>(N * N, 1); 
-        parents = vector<int>(N * N);
-        for(int i = 0; i < N * N; ++i) {
-            parents[i] = i; 
-        }
-        int ans = 1; // 4
+        this->N = grid.size();
+        this->grid =grid;
+        this->labels = vector<vector<int>>(N, vector<int>(N, 0));
+
+        int ans = 1;
+        unordered_map<int, int> sizes;
+        int seq = 1;
         for(int r = 0; r < N; ++r) {
             for(int c = 0; c < N; ++c) {
-                if(grid[r][c]) {
-                    for(int k = 0; k < 4; ++k) {
-                        int nr = r + dr[k];
-                        int nc = c + dc[k];
-                        if(inRange(nr, nc) && grid[nr][nc]) {
-                            int mergedSize = merge(r * N + c, nr * N + nc);
-                            ans = max(ans, mergedSize);
-                        }
-                    }
+                if(grid[r][c] && !labels[r][c]) {
+                    labels[r][c] = seq;
+                    sizes[seq] = dfs(r, c, seq);
+                    ans = max(ans, sizes[seq]);
+                    ++seq;
                 }
             }
         }
-/*
-1 1
-1 1
-sizes = [1, 4, 1, 1]
-parents = [1, 1, 1, 1]
-*/
         for(int r = 0; r < N; ++r) {
             for(int c = 0; c < N; ++c) {
-                if(!grid[r][c]) {
-                    set<int> setIds;
-                    for(int k = 0; k < 4; ++k) {
-                        int nr = r + dr[k];
-                        int nc = c + dc[k];
-                        if(inRange(nr, nc) && grid[nr][nc]) {
-                            setIds.insert(find(nr * N + nc));
-                        }
-                    }
-                    int sumSize = 1; // 4
-                    for(int setId : setIds) {
-                        sumSize += sizes[setId];
-                    }
-                    ans = max(ans, sumSize);
+                if(grid[r][c]) {
+                    continue;
                 }
+                set<int> uniqueLabels;
+                for(int i = 0; i < 4; ++i) {
+                    int nr = r + dr[i];
+                    int nc = c + dc[i];
+                    if(!inRange(nr, nc) || !labels[nr][nc]) {
+                        continue;
+                    }
+                    uniqueLabels.insert(labels[nr][nc]);
+                }
+                int mergedSize = 1;
+                for(int label : uniqueLabels) {
+                    mergedSize += sizes[label];
+                }
+                ans = max(ans, mergedSize);
             }
         }
         return ans;

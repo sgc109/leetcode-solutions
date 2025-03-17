043dc9c10ddfbe078a0c0599ebbb5fc92cb1f926
Wood Hwang <sh.hwang@bucketplace.net>
2025-03-18 00:02:38 +0900

    Time: 174 ms (78.82%), Space: 117 MB (66.51%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -2,32 +2,32 @@ class Solution {
     int dr[4] = {-1, 0, 1, 0};
     int dc[4] = {0, 1, 0, -1};
     int N;
-    vector<vector<int>> labels;
     vector<vector<int>> grid;
+    vector<vector<int>> labels;
     bool inRange(int r, int c) {
         return 0 <= r && r < N && 0 <= c && c < N;
     }
     int dfs(int r, int c, int label) {
-        int cnt = 1;
+        int ret = 1; // 1
         for(int i = 0; i < 4; ++i) {
             int nr = r + dr[i];
             int nc = c + dc[i];
-            if(inRange(nr, nc) && grid[nr][nc] && !labels[nr][nc]) {
-                labels[nr][nc] = label;
-                cnt += dfs(nr, nc, label);
+            if(!inRange(nr, nc) || !grid[nr][nc] || labels[nr][nc]) {
+                continue;
             }
+            labels[nr][nc] = label;
+            ret += dfs(nr, nc, label);
         }
-        return cnt;
+        return ret;
     }
 public:
     int largestIsland(vector<vector<int>>& grid) {
         this->N = grid.size();
-        this->grid =grid;
-        this->labels = vector<vector<int>>(N, vector<int>(N, 0));
-
-        int ans = 1;
+        this->grid = grid;
+        labels = vector<vector<int>>(N, vector<int>(N, 0));
         unordered_map<int, int> sizes;
-        int seq = 1;
+        int ans = 1; // 1
+        int seq = 1; // 3
         for(int r = 0; r < N; ++r) {
             for(int c = 0; c < N; ++c) {
                 if(grid[r][c] && !labels[r][c]) {
@@ -38,21 +38,32 @@ public:
                 }
             }
         }
+/*
+grid =
+1 1
+1 1
+
+labels=
+1 1
+1 1
+
+sizes = {1: 4}
+*/
         for(int r = 0; r < N; ++r) {
             for(int c = 0; c < N; ++c) {
                 if(grid[r][c]) {
                     continue;
                 }
-                set<int> uniqueLabels;
+                set<int> uniqueLabels; // {1, 2}
                 for(int i = 0; i < 4; ++i) {
                     int nr = r + dr[i];
                     int nc = c + dc[i];
-                    if(!inRange(nr, nc) || !labels[nr][nc]) {
+                    if(!inRange(nr, nc) || !grid[nr][nc]) {
                         continue;
                     }
                     uniqueLabels.insert(labels[nr][nc]);
                 }
-                int mergedSize = 1;
+                int mergedSize = 1; 
                 for(int label : uniqueLabels) {
                     mergedSize += sizes[label];
                 }

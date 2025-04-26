43a2a25ce2b551341fade9d33cfba16efb7aedc8
Wood Hwang <sh.hwang@bucketplace.net>
2025-04-26 16:24:40 +0900

    Time: 18 ms (60.41%), Space: 11.1 MB (75.81%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -10,17 +10,17 @@ class Solution {
     }
 public:
     int change(int amount, vector<int>& coins) {
-        dp = vector<vector<ll>>(coins.size() + 1, vector<ll>(amount + 1, 0));
+        dp = vector<vector<ll>>(2, vector<ll>(amount + 1, 0));
         dp[0][0] = 1;
         for(int i = 1; i <= coins.size(); ++i) {
             for(int j = 0; j <= amount; ++j) {
-                dp[i][j] = dp[i - 1][j];
+                dp[i & 1][j] = dp[~i & 1][j];
                 if(j >= coins[i - 1]) {
-                    dp[i][j] += dp[i][j - coins[i - 1]];
-                    dp[i][j] = truncateToInt(dp[i][j]);
+                    dp[i & 1][j] += dp[i & 1][j - coins[i - 1]];
+                    dp[i & 1][j] = truncateToInt(dp[i & 1][j]);
                 }
             }
         }
-        return dp[coins.size()][amount];
+        return dp[coins.size() & 1][amount];
     }
 };

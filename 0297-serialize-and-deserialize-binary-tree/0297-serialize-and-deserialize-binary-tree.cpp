e0a06390fc4409d109e2e03aac0ece7f4a540bc9
Wood Hwang <sh.hwang@bucketplace.net>
2025-04-26 14:47:43 +0900

    Time: 15 ms (98.87%), Space: 33.3 MB (50.71%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -6,23 +6,23 @@
  *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
-
- 1l2ur3l4ur5uu
  */
 class Codec {
-    void encode(TreeNode* cur, string& encoded) {
+    void dfs(TreeNode* cur, string& encoded) {
         encoded += to_string(cur->val);
         if(cur->left) {
-            encoded += 'l';
-            encode(cur->left, encoded);
+            encoded += ",";
+            dfs(cur->left, encoded);
+        } else {
+            encoded += ",x";
         }
         if(cur->right) {
-            encoded += 'r';
-            encode(cur->right, encoded);
+            encoded += ",";
+            dfs(cur->right, encoded);
+        } else {
+            encoded += ",x";
         }
-        encoded += 'u';
     }
-
     TreeNode* buildTree(string& encoded, int& idx) {
         int num = 0;
         bool isNegative = false;
@@ -35,28 +35,28 @@ class Codec {
             ++idx;
         }
         TreeNode* cur = new TreeNode(isNegative ? -num : num);
-        if(encoded[idx] == 'l') {
-            ++idx;
-            cur->left = buildTree(encoded, idx);
-        }
-        if(encoded[idx] == 'r') {
-            ++idx;
-            cur->right = buildTree(encoded, idx);
-        }
-
         ++idx;
+        if(encoded[idx] != 'x') {
+            cur->left = buildTree(encoded, idx);
+        } else {
+            idx += 2;
+        }
+        if(encoded[idx] != 'x') {
+            cur->right = buildTree(encoded, idx);
+        } else {
+            idx += 2;
+        }
         return cur;
     }
 public:
-
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         if(!root) {
             return "";
         }
-        string ret = "";
-        encode(root, ret);
-        return ret;
+        string encoded = "";
+        dfs(root, encoded);
+        return encoded;
     }
 
     // Decodes your encoded data to tree.

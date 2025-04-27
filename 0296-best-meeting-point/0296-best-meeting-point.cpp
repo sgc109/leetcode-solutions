93fda71c5a1c6535e2cd050745af8cda4e666022
Wood Hwang <sh.hwang@bucketplace.net>
2025-04-27 16:10:41 +0900

    Time: 0 ms (100%), Space: 16.9 MB (9.04%) - LeetHub

--- 0296-best-meeting-point/0296-best-meeting-point.cpp
+++ 0296-best-meeting-point/0296-best-meeting-point.cpp
@@ -4,49 +4,27 @@ public:
         int R = grid.size();
         int C = grid[0].size();
         vector<pair<int,int>> homes;
-        vector<int> countR(R, 0);
-        vector<int> countC(C, 0);
-        int sumDistR = 0;
-        int sumDistC = 0;
+        vector<int> rows;
+        vector<int> cols;
         for(int i = 0; i < R; ++i) {
             for(int j = 0; j < C; ++j) {
                 if(grid[i][j]) {
                     homes.emplace_back(i, j);
-                    countR[i]++;
-                    countC[j]++;
-                    sumDistR += i;
-                    sumDistC += j;
+                    rows.push_back(i);
+                }
+            }
+        }
+        for(int i = 0; i < C; ++i) {
+            for(int j = 0; j < R; ++j) {
+                if(grid[j][i]) {
+                    cols.push_back(i);
                 }
             }
         }
         int N = homes.size();
-        int cntR = countR[0];
-        int minDR = sumDistR;
-        int ansR = 0;
-        for(int i = 1; i < R; ++i) {
-            sumDistR -= N - cntR;
-            sumDistR += cntR;
-            if(minDR > sumDistR) {
-                minDR = sumDistR;
-                ansR = i;
-            }
-            cntR += countR[i];
-        }
-        int cntC = countC[0];
-        int minDC = sumDistC;
-        int ansC = 0;
-        for(int i = 1; i < C; ++i) {
-            sumDistC -= N - cntC;
-            sumDistC += cntC;
-            if(minDC > sumDistC) {
-                minDC = sumDistC;
-                ansC = i;
-            }
-            cntC += countC[i];
-        }
         int ans = 0;
         for(auto& p : homes) {
-            ans += abs(p.first - ansR) + abs(p.second - ansC);
+            ans += abs(p.first - rows[rows.size() / 2]) + abs(p.second - cols[cols.size() / 2]);
         }
         return ans;
     }

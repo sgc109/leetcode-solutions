473d328a17c48b2492a4a0198c4395ec47b6981d
Wood Hwang <sh.hwang@bucketplace.net>
2025-04-28 23:12:45 +0900

    Time: 0 ms (100%), Space: 16.2 MB (49.83%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.cpp
+++ 0529-minesweeper/0529-minesweeper.cpp
@@ -3,25 +3,30 @@ class Solution {
     bool inRange(int r, int c) {
         return 0 <= r && r < R && 0 <= c && c < C;
     }
-    int countMines(int r, int c, vector<vector<char>>& board) {
-        int ret = 0;
-        for(int i = 0; i < 8; ++i) {
-            int nr = r + "00122210"[i] - '1';
-            int nc = c + "12221000"[i] - '1';
-            if(inRange(nr, nc) && board[nr][nc] == 'M') {
-                ++ret;
-            }
-        }
-        return ret;
-    }
-    bool reveal(int r, int c, vector<vector<char>>& board) {
-        int cntMines = countMines(r, c, board);
+    bool reveal(int r, int c, vector<vector<int>>& mineCountMap, vector<vector<char>>& board) {
+        int cntMines = mineCountMap[r][c];
         if(cntMines > 0) {
             board[r][c] = cntMines + '0';
             return false;
         } else {
             board[r][c] = 'B';
             return true;
+        }
+    }
+    void countMines(vector<vector<int>>& mineCountMap, vector<vector<char>>& board) {
+        for(int r = 0; r < R; ++r) {
+            for(int c = 0; c < C; ++c) {
+                if(board[r][c] != 'M') {
+                    continue;
+                }
+                for(int k = 0; k < 8; ++k) {
+                    int nr = r + "00122210"[k] - '1';
+                    int nc = c + "12221000"[k] - '1';
+                    if(inRange(nr, nc)) {
+                        mineCountMap[nr][nc]++;
+                    }
+                }
+            }
         }
     }
 public:
@@ -32,8 +37,10 @@ public:
         }
         this->R = board.size();
         this->C = board[0].size();
+        vector<vector<int>> mineCountMap = vector<vector<int>>(R, vector<int>(C, 0));
+        countMines(mineCountMap, board);
         queue<pair<int,int>> q;
-        if(reveal(click[0], click[1], board)) {
+        if(reveal(click[0], click[1], mineCountMap, board)) {
             q.push({click[0], click[1]});
         }
         while(!q.empty()) {
@@ -45,7 +52,7 @@ public:
                 if(!inRange(nxtR, nxtC) || board[nxtR][nxtC] != 'E') {
                     continue;
                 }
-                if(reveal(nxtR, nxtC, board)) {
+                if(reveal(nxtR, nxtC, mineCountMap, board)) {
                     q.push({nxtR, nxtC});
                 }
             }

8900e2e5afb4b3dc37ffb9d1aa4ca6b24bf8278e
Sean Hwang <sgc109109@gmail.com>
2026-06-19 05:31:59 +0100

    Time: 156 ms (28.6%), Space: 73 MB (22.13%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    bool isPossibleDivide(vector<int>& nums, int k) { // k = 4
-        map<int, int> counter; // {1:1, 2:1, 3:2, 4:2, 5:1, 6:1}
-        queue<pair<int, int>> delta; // {4:-1, }
-
-
+    bool isPossibleDivide(vector<int>& nums, int k) {
+        map<int, int> counter;
         for(int num : nums) {
             counter[num]++;
         }
-        
-        int maxVal = counter.rbegin()->first; // maxVal = 6
-        int minus = 0; // minus = 1
-        int prev = -1; // prev = 2
-        for(auto [num, cnt] : counter) { // num = 2, cnt = 1
-            if(prev != -1 && prev != num - 1 && minus > 0) {
-                return false;
+        for(auto [num, cnt] : counter) {
+            if(cnt == 0) {
+                continue;
             }
-            prev = num;
-            if(cnt < minus) {
-                return false;
-            } else if(cnt > minus) {
-                int add = cnt - minus; // add = 1
-                if(num + k > maxVal + 1 || counter[num + k - 1] == 0) {
+            for(int i = num; i < num + k; ++i) {
+                if(counter[i] < cnt) {
                     return false;
                 }
-                minus = cnt;
-                delta.push({num + k - 1, -add});
-            }
-            while(!delta.empty() && delta.front().first <= num) {
-                minus += delta.front().second;
-                delta.pop();
+                counter[i] -= cnt;
             }
         }
         return true;

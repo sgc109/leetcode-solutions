7195732d8a6c6d2a143414d16a986aec0f601c77
Sean Hwang <sgc109109@gmail.com>
2026-06-27 20:14:23 +0100

    Time: 220 ms (26.72%), Space: 173.6 MB (7.02%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        sort(begin(changed), end(changed));
-        unordered_map<int, int> counter;
         vector<int> ans;
+        unordered_map<int, int> in;
+        unordered_map<int, int> counter;
+
         for(int num : changed) {
-            if(num % 2 == 0 && counter.find(num / 2) != counter.end()) {
-                counter[num / 2]--;
-                ans.push_back(num / 2);
-                if(counter[num / 2] == 0) {
-                    counter.erase(num / 2);
-                }
-            } else {
-                counter[num]++;
-            }
+            counter[num]++;
+            in[num * 2]++;
         }
-        if(counter.size() > 0) {
+        if(counter[0] % 2) {
             return vector<int>();
         }
+        for(int i = 0; i < counter[0] / 2; ++i) {
+            ans.push_back(0);
+        }
+        for(int num : changed) {
+            if(in[num] || num == 0) {
+                continue;
+            }
+            int cur = num;
+            while(counter[cur]) {
+                if(counter[cur * 2] < counter[cur]) {
+                    return vector<int>();
+                }
+                for(int i = 0; i < counter[cur]; ++i) {
+                    ans.push_back(cur);       
+                }
+                counter[cur * 2] -= counter[cur];
+                counter[cur] = 0;
+                if(counter[cur * 2] == 0) {
+                    cur *= 4;
+                } else {
+                    cur *= 2;
+                }
+            }
+        }
+
         return ans;
     }
 };

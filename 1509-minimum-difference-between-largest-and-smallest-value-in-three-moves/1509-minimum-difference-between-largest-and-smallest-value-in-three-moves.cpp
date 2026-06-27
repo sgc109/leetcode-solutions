ba7baf22f88f983bbfe2e869d73aa48ad0667daa
Sean Hwang <sgc109109@gmail.com>
2026-06-28 00:38:04 +0100

    Time: 0 ms (100%), Space: 42.5 MB (53.8%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,16 +1,41 @@
 class Solution {
+    void updateMin4(vector<int>& min4, int newNum) {
+        int cur = newNum;
+        for(int i = 0; i < 4; ++i) {
+            if(min4[i] > cur) {
+                swap(min4[i], cur);
+            }
+        }
+    }
+
+    void updateMax4(vector<int>& max4, int newNum) {
+        int cur = newNum;
+        for(int i = 0; i < 4; ++i) {
+            if(max4[i] < cur) {
+                swap(max4[i], cur);
+            }
+        }
+    }
 public:
     int minDifference(vector<int>& nums) {
-        int N = nums.size();
-        if(N <= 4) {
+        if(nums.size() <= 4) {
             return 0;
         }
-        sort(begin(nums), end(nums));
-        int ans = 2e9 + 1;
+        int maxVal = 1e9;
+        vector<int> min4({maxVal, maxVal, maxVal, maxVal});
+        vector<int> max4({-maxVal, -maxVal, -maxVal, -maxVal});
+
+        for(int num : nums) {
+            updateMin4(min4, num);
+            updateMax4(max4, num);
+        }
+
+        int ans = 2e9;
         for(int i = 0; i < 4; ++i) {
-            int diff = nums[N - 4 + i] - nums[i];
+            int diff = max4[i] - min4[3 - i];
             ans = min(ans, diff);
         }
+        
         return ans;
     }
 };
